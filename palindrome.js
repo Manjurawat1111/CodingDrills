e62c96fb9e148c6d032faee97323255dc307dcfe
Manju Rawat <manjurawat1111@gmail.com>
2023-08-16 01:05:41 +0530

    init

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,16 @@
-const string='level';
-
-
-const newString=string.split("").reverse().join("");
-console.log(newString);
-
-if(string===newString)
+const string='racecar';
+// const newString=string.split("").reverse().join("");
+// console.log(newString);
+function stringReversed(string){
+    let newString="";
+    for(let i=string.length-1;i>=0;i--){
+        newString+=string[i];
+    }
+    return newString;
+}
+const result=stringReversed(string);
+if(string===result)
 console.log(`yes ${string} is a palindrome`)
 else
 console.log(`No ${string} is not a palindrome` )
+
