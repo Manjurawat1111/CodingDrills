010f9ab1e5e9452aec81c7f5c3c36e697ce3d5ac
Manju Rawat <manjurawat1111@gmail.com>
2023-08-15 21:36:01 +0530

    init

---
+++ strings.js
@@ -0,0 +1,17 @@
+// const favFruitArray=["orange","Apple","Banana","Guava"];
+// const readLineSync=require("readline-sync");
+// const userFav=readLineSync.question("Which is your fav fruit?");
+
+// const isFruitAvailable= favFruitArray.includes(userFav);
+// if(isFruitAvailable)
+// console.log("yes fruit is available");
+// else
+// console.log("No fruit is not available");
+const array=[1,2,7,5,8,6,3,4];
+console.log(array);
+const sortArray=array.sort();
+
+
+console.log(sortArray);
+
+
